a7c8715e4ec38829ee7389f3830bf5854b3f6f26
Fumiakioe <king_crimson_1111@yahoo.co.jp>
2020-03-04 21:52:06 +0900

    use

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
+# ルーティングを追加
 Rails.application.routes.draw do
-
-
   devise_for :users
+  # 下の行は削除する
+  # get 'messages/index'
+
   root "messages#index"
+  resources :users, only: [:edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
